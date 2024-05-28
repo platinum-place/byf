790d6c908fccf3e6110618fa4fcd260724c570d1
warlyn <warlyn200@gmail.com>
2024-05-28 15:26:17 -0400

    update user

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,20 +18,40 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    public static function getModelLabel(): string
+    {
+        return __('User');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('Users');
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Utilities');
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
+                    ->translateLabel()
                     ->required(),
                 Forms\Components\TextInput::make('email')
+                    ->translateLabel()
                     ->email()
                     ->required(),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\DateTimePicker::make('email_verified_at')
+                    ->translateLabel(),
                 Forms\Components\TextInput::make('password')
+                    ->translateLabel()
                     ->password()
                     ->required(),
                 Forms\Components\Toggle::make('is_admin')
+                    ->translateLabel()
                     ->required(),
             ]);
     }
@@ -41,19 +61,25 @@ class UserResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
+                    ->translateLabel()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
+                    ->translateLabel()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email_verified_at')
+                    ->translateLabel()
                     ->dateTime()
                     ->sortable(),
                 Tables\Columns\IconColumn::make('is_admin')
+                    ->translateLabel()
                     ->boolean(),
                 Tables\Columns\TextColumn::make('created_at')
+                    ->translateLabel()
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->translateLabel()
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
